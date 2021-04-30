0872a6cf117d7c670df27455da9a885ef76293c5
Aaron Francis <aarondfrancis@gmail.com>
2021-04-30 17:53:17 -0500

    Config changes

--- config/sidecar.php
+++ config/sidecar.php
@@ -9,29 +9,15 @@ return [
         // \App\Sidecar\ProcessThumbnail::class,
     ],
 
-    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-     *                                                                     *
-     *  You are welcome to edit this configuration directly, or you can    *
-     *  run `php artisan sidecar:env` for an interactive walk-through.   *
-     *                                                                     *
-     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+     *                                                                       *
+     *  You are welcome to edit this configuration directly, or you can run  *
+     *  `php artisan sidecar:configure` for an interactive walk-through.     *
+     *                                                                       *
+     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
     /*
-     * Your AWS key. It will need the following permissions (at a minimum):
-     * {
-     *     "Version": "2012-10-17",
-     *     "Statement": [
-     *         {
-     *             "Effect": "Allow",
-     *             "Action": [
-     *                 "s3:*",
-     *                 "lambda:*"
-     *             ],
-     *             "Resource": "*"
-     *         }
-     *     ]
-     * }
+     * Your AWS key. See CreateDeploymentUser::policy for the IAM policy.
      *
      * Unfortunately you cannot rely on the keys available in the Vapor
      * runtime, as those do not have the right permissions.
@@ -44,39 +30,20 @@ return [
     'aws_secret' => env('SIDECAR_SECRET_ACCESS_KEY'),
 
     /*
-     * The region where your Lambdas will be deployed. You can provide
-     * your own or leave it blank to use the same region Vapor uses.
+     * The region where your Lambdas will be deployed.
      */
     'aws_region' => env('SIDECAR_REGION'),
 
     /*
-     * The bucket that temporarily holds your function's ZIP files for deployment
-     * to Lambda. It must be in the same region as your Lambdas. You can
-     * specify your own, or default to the same one Vapor uses.
+     * The bucket that temporarily holds your function's ZIP files as they
+     * are deployed to Lambda. It must be the same region as your Lambdas.
      */
     'aws_bucket' => env('SIDECAR_ARTIFACT_BUCKET_NAME'),
 
     /*
      * This is the execution role that your Lambdas will use.
      *
-     * If you want to create your own role, you can modify the IAM Policy
-     * template below to meet your needs:
-     * {
-     *     "Version": "2012-10-17",
-     *     "Statement": [
-     *         {
-     *             "Action": [
-     *                 "lambda:invokeFunction",
-     *                 "s3:*",
-     *                 "ses:*",
-     *                 "sqs:*",
-     *                 "dynamodb:*"
-     *             ],
-     *             "Effect": "Allow",
-     *             "Resource": "*"
-     *         }
-     *     ]
-     * }
+     * See CreateExecutionRole::policy for the IAM policy.
      */
     'execution_role' => env('SIDECAR_EXECUTION_ROLE'),
 
