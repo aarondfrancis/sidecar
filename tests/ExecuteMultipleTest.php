5a5caf28001ec4de52a5837391c3bae9d0004673
Aaron Francis <aarondfrancis@gmail.com>
2021-05-04 21:34:21 -0500

    Execute multiple test

--- tests/ExecuteMultipleTest.php
+++ tests/ExecuteMultipleTest.php
@@ -6,16 +6,19 @@
 namespace Hammerstone\Sidecar\Tests;
 
 use Aws\Result;
+use GuzzleHttp\Promise\PromiseInterface;
 use Hammerstone\Sidecar\Clients\LambdaClient;
-use Hammerstone\Sidecar\LambdaFunction;
+use Hammerstone\Sidecar\Results\PendingResult;
 use Hammerstone\Sidecar\Sidecar;
+use Hammerstone\Sidecar\Tests\Support\EmptyTestFunction;
+use Mockery;
 
 class ExecuteMultipleTest extends BaseTest
 {
     protected function expectedArgs($args = [])
     {
         return array_merge([
-            'FunctionName' => 'SC-Laravel-testing-Hammerstone-Sidecar-Tests-TestFunction:active',
+            'FunctionName' => 'SC-Laravel-testing-stone-Sidecar-Tests-Support-EmptyTestFunction:active',
             'InvocationType' => 'RequestResponse',
             'LogType' => 'Tail',
             'Payload' => '[]'
@@ -24,7 +27,10 @@ class ExecuteMultipleTest extends BaseTest
 
     protected function mockMultiple()
     {
-        $result = new Result;
+        $result = Mockery::mock(PromiseInterface::class)
+            ->shouldReceive('wait')
+            ->andReturn(new Result)
+            ->getMock();
 
         $mock = $this->mock(LambdaClient::class);
 
@@ -55,7 +61,7 @@ class ExecuteMultipleTest extends BaseTest
     {
         $this->mockMultiple();
 
-        TestFunction::executeMany([[
+        EmptyTestFunction::executeMany([[
             'A' => 1
         ], [
             'B' => 2
@@ -69,7 +75,7 @@ class ExecuteMultipleTest extends BaseTest
     {
         $this->mockMultiple();
 
-        Sidecar::executeMany(TestFunction::class, [[
+        Sidecar::executeMany(EmptyTestFunction::class, [[
             'A' => 1
         ], [
             'B' => 2
@@ -83,7 +89,7 @@ class ExecuteMultipleTest extends BaseTest
     {
         $this->mockMultiple();
 
-        Sidecar::executeMany(new TestFunction, [[
+        Sidecar::executeMany(new EmptyTestFunction, [[
             'A' => 1
         ], [
             'B' => 2
@@ -95,52 +101,65 @@ class ExecuteMultipleTest extends BaseTest
     /** @test */
     public function execute_many_by_function_int()
     {
-        $result = new Result;
+        $result = Mockery::mock(PromiseInterface::class)
+            ->shouldReceive('wait')
+            ->andReturn(new Result)
+            ->getMock();
 
         $this->mock(LambdaClient::class)
             ->shouldReceive('invokeAsync')
             ->times(5)
             ->andReturn($result);
 
-        TestFunction::executeMany(5);
+        EmptyTestFunction::executeMany(5);
     }
 
     /** @test */
     public function execute_many_by_facade_int()
     {
-        $result = new Result;
+        $result = Mockery::mock(PromiseInterface::class)
+            ->shouldReceive('wait')
+            ->andReturn(new Result)
+            ->getMock();
 
         $this->mock(LambdaClient::class)
             ->shouldReceive('invokeAsync')
             ->times(5)
             ->andReturn($result);
 
-        Sidecar::executeMany(TestFunction::class, 5);
+        Sidecar::executeMany(EmptyTestFunction::class, 5);
     }
 
     /** @test */
     public function execute_many_by_facade_int_with_instantiated_class()
     {
-        $result = new Result;
+        $result = Mockery::mock(PromiseInterface::class)
+            ->shouldReceive('wait')
+            ->andReturn(new Result)
+            ->getMock();
 
         $this->mock(LambdaClient::class)
             ->shouldReceive('invokeAsync')
             ->times(5)
             ->andReturn($result);
 
-        Sidecar::executeMany(new TestFunction, 5);
-    }
-}
-
-class TestFunction extends LambdaFunction
-{
-    public function handler()
-    {
-        //
+        Sidecar::executeMany(new EmptyTestFunction, 5);
     }
 
-    public function package()
+    /** @test */
+    public function execute_many_async_by_function()
     {
-        //
+        $result = Mockery::mock(PromiseInterface::class)
+            ->shouldNotReceive('wait')
+            ->getMock();
+
+        $this->mock(LambdaClient::class)
+            ->shouldReceive('invokeAsync')
+            ->times(2)
+            ->andReturn($result);
+
+        $results = EmptyTestFunction::executeMany(2, $async = true);
+
+        $this->assertInstanceOf(PendingResult::class, $results[0]);
     }
 }
