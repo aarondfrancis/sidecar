5810a267f21995a8f01dc6b7ae0b4686d5fe24c8
Aaron Francis <aarondfrancis@users.noreply.github.com>
2021-05-24 21:43:43 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/Commands/ConfigureTest.php
+++ tests/Commands/ConfigureTest.php
@@ -126,20 +126,18 @@ class ConfigureTest extends BaseTest
                     ]]
                 ]),
             ]);
-
     }
-
 
     protected function roleExistsResponse()
     {
         return [
-            "Role" => [
-                "Path" => "/",
-                "RoleName" => "sidecar-execution-role",
-                "RoleId" => "XXX",
-                "Arn" => "arn:aws:iam::XXX:role/sidecar-execution-role",
-                "AssumeRolePolicyDocument" => "%7B%22Version%22%3A%222012-10-17%22%2C%22Statement%22%3A%5B%7B%22Effect%22%3A%22Allow%22%2C%22Principal%22%3A%7B%22Service%22%3A%22lambda.amazonaws.com%22%7D%2C%22Action%22%3A%22sts%3AAssumeRole%22%7D%5D%7D",
-                "MaxSessionDuration" => 3600,
+            'Role' => [
+                'Path' => '/',
+                'RoleName' => 'sidecar-execution-role',
+                'RoleId' => 'XXX',
+                'Arn' => 'arn:aws:iam::XXX:role/sidecar-execution-role',
+                'AssumeRolePolicyDocument' => '%7B%22Version%22%3A%222012-10-17%22%2C%22Statement%22%3A%5B%7B%22Effect%22%3A%22Allow%22%2C%22Principal%22%3A%7B%22Service%22%3A%22lambda.amazonaws.com%22%7D%2C%22Action%22%3A%22sts%3AAssumeRole%22%7D%5D%7D',
+                'MaxSessionDuration' => 3600,
             ],
         ];
     }
@@ -160,7 +158,6 @@ class ConfigureTest extends BaseTest
             ]),
         ];
     }
-
 
     /** @test */
     public function basic_happy_path()
@@ -310,7 +307,7 @@ class ConfigureTest extends BaseTest
             ->andReturn([
                 'AccessKey' => [
                     'AccessKeyId' => 'AK-XXXX',
-                    'SecretAccessKey' => "SK-XXXX"
+                    'SecretAccessKey' => 'SK-XXXX'
                 ]
             ]);
     }
