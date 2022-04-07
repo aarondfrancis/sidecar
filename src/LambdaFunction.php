0e6fa9decac047fb2f62536cf932ec63f57c7c1c
Aaron Francis <Aarondfrancis@gmail.com>
2022-04-07 14:05:11 -0500

    Add LambdaFunction back as an empty shim

---
+++ src/LambdaFunction.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Aaron Francis <aaron@hammerstone.dev|https://twitter.com/aarondfrancis>
+ */
+
+namespace Hammerstone\Sidecar;
+
+/**
+ * @see ServerlessFunction
+ * @deprecated Use ServerlessFunction instead.
+ */
+abstract class LambdaFunction extends ServerlessFunction
+{
+    //
+}
