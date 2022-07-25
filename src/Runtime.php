e42ff9f397332fd63cc3e31964411911ecbff9da
aarondfrancis <aarondfrancis@users.noreply.github.com>
2022-07-25 16:04:38 +0000

    Fix code styling [ci skip]

--- src/Runtime.php
+++ src/Runtime.php
@@ -5,22 +5,40 @@ namespace Hammerstone\Sidecar;
 abstract class Runtime
 {
     public const NODEJS_16 = 'nodejs16.x';
+
     public const NODEJS_14 = 'nodejs14.x';
+
     public const NODEJS_12 = 'nodejs12.x';
+
     public const NODEJS_10 = 'nodejs10.x';
+
     public const PYTHON_39 = 'python3.9';
+
     public const PYTHON_38 = 'python3.8';
+
     public const PYTHON_37 = 'python3.7';
+
     public const PYTHON_36 = 'python3.6';
+
     public const PYTHON_27 = 'python2.7';
+
     public const RUBY_27 = 'ruby2.7';
+
     public const RUBY_25 = 'ruby2.5';
+
     public const JAVA_11 = 'java11';
+
     public const JAVA_8_LINUX2 = 'java8.al2';
+
     public const JAVA_8 = 'java8';
+
     public const GO_1X = 'go1.x';
+
     public const DOT_NET_6 = 'dotnet6';
+
     public const DOT_NET_31 = 'dotnetcore3.1';
+
     public const DOT_NET_21 = 'dotnetcore2.1';
+
     public const PROVIDED_AL2 = 'provided.al2';
 }
