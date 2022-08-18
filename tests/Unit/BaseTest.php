b19d7d8d5a7a591c7ce28ca65c645ec6e8c31706
Wilsen Hernández <wilsenh95@gmail.com>
2022-08-17 22:25:26 -0400

    Define the variable in the base test

--- tests/Unit/BaseTest.php
+++ tests/Unit/BaseTest.php
@@ -10,6 +10,17 @@ use Orchestra\Testbench\TestCase;
 
 abstract class BaseTest extends TestCase
 {
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+    */
+    protected function defineEnvironment($app)
+    {
+        $app['config']->set('sidecar.app_name', 'Laravel');
+    }
+
     protected function getPackageProviders($app)
     {
         return [
