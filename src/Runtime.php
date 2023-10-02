33080f189bbf5ee6650533d5453733c92c824ef4
Jørgensen <kristian@froelund.net>
2023-10-02 13:34:39 +0200

    Chore: Runtime update

--- src/Runtime.php
+++ src/Runtime.php
@@ -4,11 +4,15 @@ namespace Hammerstone\Sidecar;
 
 abstract class Runtime
 {
+    public const NODEJS_18 = 'nodejs18.x';
+
     public const NODEJS_16 = 'nodejs16.x';
 
     public const NODEJS_14 = 'nodejs14.x';
 
-    public const NODEJS_12 = 'nodejs12.x';
+    public const PYTHON_311 = 'python3.11';
+
+    public const PYTHON_310 = 'python3.10';
 
     public const PYTHON_39 = 'python3.9';
 
@@ -18,6 +22,8 @@ abstract class Runtime
 
     public const RUBY_27 = 'ruby2.7';
 
+    public const JAVA_17 = 'java17';
+
     public const JAVA_11 = 'java11';
 
     public const JAVA_8_LINUX2 = 'java8.al2';
@@ -26,9 +32,9 @@ abstract class Runtime
 
     public const GO_1X = 'go1.x';
 
-    public const DOT_NET_6 = 'dotnet6';
+    public const DOT_NET_7 = 'dotnet7';
 
-    public const DOT_NET_31 = 'dotnetcore3.1';
+    public const DOT_NET_6 = 'dotnet6';
 
     public const PROVIDED_AL2 = 'provided.al2';
 }
